932271849d513a8d16ad95ea81e1557756fe5860
Jiacheng Zou <zihutanejo0409@hotmail.com>
2017-06-06 20:40:20 -0700

    Adjust Sharpe ratio calculation's mistake.

---
+++ high_frequency.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 06 20:29:07 2017
+
+@author: Jiacheng Z
+"""
+#----this correspond to question 4----
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.linalg import inv as invert
+from vanillaPCA import getFileLocs
+import pandas as pd
+pd.read_csv()
