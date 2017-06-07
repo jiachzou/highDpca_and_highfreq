28c22a25c6e12e77621b5f63e51eecc362a26d8f
Jiacheng Zou <zihutanejo0409@hotmail.com>
2017-06-06 22:15:12 -0700

    Add 'Daily Spot' high-frequency volatilities' calculations.

--- high_frequency.py
+++ high_frequency.py
@@ -18,17 +18,18 @@ def main():
     rwData = pd.read_csv(file_loc)
 
     SPY = rwData.iloc[:,0].as_matrix()
-    DeltaSPY = np.diff(SPY, n=1)
-    Sq_DeltaSPY = np.square(DeltaSPY)
-    VolLenSPY = len(SPY)//N_WeeklyObs +1
-    VolSPY = np.array([0.0]*VolLenSPY)
-
     AAPL = rwData.iloc[:, 1].as_matrix()
+    DeltaSPY = np.diff(SPY, n=1)
     DeltaAAPL = np.diff(AAPL, n=1)
+    Sq_DeltaSPY = np.square(DeltaSPY)
     Sq_DeltaAAPL = np.square(DeltaAAPL)
-    VolAAPL = np.array([0.0]*VolLenSPY)
+    VolLenWeekly = len(SPY)//N_WeeklyObs +1
 
-    for idx in range(VolLenSPY):
+    WeeklySpotVol = dict()
+    WeeklySpotVol['SPY'] = np.array([0.0] * VolLenWeekly)
+    WeeklySpotVol['AAPL'] = np.array([0.0] * VolLenWeekly)
+
+    for idx in range(VolLenWeekly):
         if idx+N_WeeklyObs>len(SPY):
             WeeklyChunkSPY = Sq_DeltaSPY[idx*N_WeeklyObs:]
             WeeklyChunkAAPL = Sq_DeltaAAPL[idx*N_WeeklyObs:]
@@ -36,17 +37,48 @@ def main():
             WeeklyChunkSPY = Sq_DeltaSPY[idx*N_WeeklyObs:(idx + 1)*N_WeeklyObs]
             WeeklyChunkAAPL = Sq_DeltaAAPL[idx*N_WeeklyObs:(idx + 1)*N_WeeklyObs]
 
-        VolSPY[idx] = np.sum(WeeklyChunkSPY)
-        VolAAPL[idx] = np.sum(WeeklyChunkAAPL)
+        WeeklySpotVol['SPY'][idx] = np.sum(WeeklyChunkSPY)
+        WeeklySpotVol['AAPL'][idx] = np.sum(WeeklyChunkAAPL)
 
     plt.figure()
-    plt.plot(VolAAPL,label='AAPL High-frequency Volatility')
-    plt.plot(VolSPY, label='SPY High-frequency Volatility')
+    plt.plot(WeeklySpotVol['AAPL'],label='AAPL High-frequency Volatility')
+    plt.plot(WeeklySpotVol['SPY'], label='SPY High-frequency Volatility')
     plt.title('Fig.7 Weeekly Volatility')
+    plt.ylabel('Quadratic Covariance')
+    plt.xlabel('No. of Week')
     plt.legend()
     plt.savefig('Fig_7.png')
 
     print('{0}\n[INFO] Finished calculating high-frequency weekly volatilities.'.
+        format('=' * 20 + NOW() + '=' * 20))
+
+    #------------part 2, qeustion 4: jump estimation-----------------
+    N_DailyObs = N_WeeklyObs/5
+    VolLenDaily = len(SPY)//N_DailyObs+1
+    DailySpotVol = dict()
+    DailySpotVol['AAPL'] = np.array([0.0] * VolLenDaily)
+    DailySpotVol['SPY'] = np.array([0.0] * VolLenDaily)
+    for idx in range(VolLenDaily):
+        if idx+N_DailyObs>len(SPY):
+            DailyChunkSPY = Sq_DeltaSPY[idx*N_DailyObs:]
+            DailyChunkAAPL = Sq_DeltaAAPL[idx*N_DailyObs:]
+        else:
+            DailyChunkSPY = Sq_DeltaSPY[idx*N_DailyObs:(idx + 1)*N_DailyObs]
+            DailyChunkAAPL = Sq_DeltaAAPL[idx*N_DailyObs:(idx + 1)*N_DailyObs]
+
+        DailySpotVol['SPY'][idx] = np.sum(DailyChunkSPY)
+        DailySpotVol['AAPL'][idx] = np.sum(DailyChunkAAPL)
+
+    plt.figure()
+    plt.plot(DailySpotVol['AAPL'],label='AAPL High-frequency Volatility')
+    plt.plot(DailySpotVol['SPY'], label='SPY High-frequency Volatility')
+    plt.title('Fig.8 Daily Spot Volatility')
+    plt.ylabel('Quadratic Covariance')
+    plt.xlabel('No. of Day')
+    plt.legend()
+    plt.savefig('Fig_8.png')
+
+    print('{0}\n[INFO] Finished calculating high-frequency daily volatilities.'.
         format('=' * 20 + NOW() + '=' * 20))
 
 
