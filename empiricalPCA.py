a00e84f36c7fc40353dc1e1218a19f74299810e8
Jiacheng Zou <zihutanejo0409@hotmail.com>
2017-06-07 02:37:59 -0700

    Add empirical parts for PCA.

---
+++ empiricalPCA.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 07 02:37:05 2017
+
+@author: Jiacheng Z
+"""
+#----this is a wrapper for code in question 3----
+Datasets = list()
+Datasets.append('')
